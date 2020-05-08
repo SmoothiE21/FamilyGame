beaa25761ebb0798713256391d995bd1d7b272a4
SmoothiE21 <32088283+SmoothiE21@users.noreply.github.com>
2020-05-08 20:22:14 +0300

    Empty Projekt
    Create Empy Projekt

---
+++ FamilyGame/FamilyGame/Main.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_NONSTDC_NO_DEPRECATE
+#define WIN32_LEAN_AND_MEAN
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <Windows.h>
+#include <time.h> 
+#include <conio.h>
+
+
+
+int main()
+{
+	printf("hello vilag\n");
+	system("pause");
+}
